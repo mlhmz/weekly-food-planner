df1cedfba6b5dead5890bfb9197d636a421b999b
Malek Hamza <malek.hamza@gmx.de>
2021-06-16 22:52:36 +0200

    initial ui commit, added bootstrap to it and started to make a layout

---
+++ ui/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Content msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Content from './components/Content.vue'
+import 'bootstrap/scss/bootstrap.scss';
+
+export default {
+  name: 'App',
+  components: {
+    Content
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+
+a {
+  margin-left: 15px;
+}
+</style>
